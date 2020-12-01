5429d5f6a26c71a42d62e45ae78ae4ff89c6eb85
Vileblood <david.thomas1986@gmail.com>
2020-12-01 16:45:59 +0000

    added helper method and keyboard events

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -14,27 +14,38 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings() 
 
-    self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height)) 
 
 
-    pygame.display.set_caption("Alien Invasion")
+        pygame.display.set_caption("Alien Invasion")
 
-    self.ship = Ship(self)
+        self.ship = Ship(self)
 
-    # set background colour
-    self.bg_color = (230, 230, 230)
+        # set background colour
+        self.bg_color = (230, 230, 230)
 
 
 def run_game(self):
     """ Start main loop for game """
     
     while True:
-        # watch for keyboard and mouse events.
+            self._check_events()
+            self._update_screen()
+
+
+    def _check_events(self): 
+        # watch for keyboard and mouse events.       
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    # Move the ship to the right.
+                    self.ship.rect.x += 1
+                        
 
+    def _update_screen(self):
     # redraw the screen during each pass of the loop (start a new game)
         self.screen.fill(self.settings.bg_color)   
         self.ship.blitme()     
