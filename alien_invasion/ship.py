ebb651492caa29a6b6afb4ef9e6131b768706460
Vileblood <david.thomas1986@gmail.com>
2020-12-21 20:43:28 +0000

    added ability for ship to stop when key is released and not move off the screen. set width and height

--- alien_invasion/ship.py
+++ alien_invasion/ship.py
@@ -7,7 +7,7 @@ class Ship:
         """ Initialize ship and set its starting position """
 
         self.screen = ai_game.screen
-        self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship and get its rect
         self.image = pygame.image.load('images/357-3576984_spaceship-cartoon-clipart.png')
@@ -17,16 +17,20 @@ class Ship:
         #Start each new ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        #Store a decimal value for the ships horizontal position
+        self.x = float(self.rect.x)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
 
     def update(self):
-        """ Update the ships position based on the movement """
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
+        """ Update the ships position based on the movement flags """
+        #Update ships x value, not the rekt
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
 
 
     def blitme(self):
